15115cadd372e49db03dfb5215a406175f15d452
BogdanAngh <bogdananghelache@gmail.com>
2019-04-06 23:33:55 +0300

    refactor 1

--- Server/routes/serviceRoutes.js
+++ Server/routes/serviceRoutes.js
@@ -6,39 +6,39 @@ const hash = require('object-hash');
 module.exports = (app) => {
     let count = 0;
     let songsLength = 0;
-    app.get('/api/songCreate', (req, res) => {
-        let songProt = {
-            name:   "Song 13",
-            artist: "Artist 13",
-            album:  "Album 13",
-        }
-        let id = hash.MD5({name: songProt.name, artist: songProt.artist, album: songProt.album})
-        let song = {
-            songId: id,
-            name:   "Song 13",
-            artist: "Artist 13",
-            album:  "Album 13",
-            url:    "https://www.youtube.com/watch?v=0_hUI_fbKe8"
-        }
-        Song.findOne({songId: song.songId})
-            .then((existingSong) => {
-                if(existingSong){
-                    let str = `The song with the name ${existingSong.name} already exists`;
-                    res.send(str);
-                }else{
-                    new Song(song)
-                    .save()
-                    .then((song) => {
-                        let string = 'Song created succesfully. here is the song';
-                        res.send(string + JSON.stringify(song), null, 4);
-                    })
-                    .catch((err) => {
-                        res.status(400).send(err);
-                    })
-                }
-            })
+    // app.get('/api/songCreate', (req, res) => {
+    //     let songProt = {
+    //         name:   "Song 13",
+    //         artist: "Artist 13",
+    //         album:  "Album 13",
+    //     }
+    //     let id = hash.MD5({name: songProt.name, artist: songProt.artist, album: songProt.album})
+    //     let song = {
+    //         songId: id,
+    //         name:   "Song 13",
+    //         artist: "Artist 13",
+    //         album:  "Album 13",
+    //         url:    "https://www.youtube.com/watch?v=0_hUI_fbKe8"
+    //     }
+    //     Song.findOne({songId: song.songId})
+    //         .then((existingSong) => {
+    //             if(existingSong){
+    //                 let str = `The song with the name ${existingSong.name} already exists`;
+    //                 res.send(str);
+    //             }else{
+    //                 new Song(song)
+    //                 .save()
+    //                 .then((song) => {
+    //                     let string = 'Song created succesfully. here is the song';
+    //                     res.send(string + JSON.stringify(song), null, 4);
+    //                 })
+    //                 .catch((err) => {
+    //                     res.status(400).send(err);
+    //                 })
+    //             }
+    //         })
 
-    });
+    // });
 
     app.get('/api/songs', (req, res) => {
         
@@ -68,8 +68,6 @@ module.exports = (app) => {
             .catch(err => {
                 res.send(err);
             })
-        Song.collection.stats((err,res) =>{
-            console.log(res.storageSize)
         })
             
     });
@@ -120,43 +118,42 @@ module.exports = (app) => {
             })
     });
 
-    app.get('/api/modifyPlaylist', (req,res) => {
-        let userName = req.query.userName;
-        let playlistName = req.query.playlistName;
-        let id = hash.MD5({name: req.query.name, artist: req.query.artist, album: req.query.album})
+    // app.get('/api/modifyPlaylist', (req,res) => {
+    //     let userName = req.query.userName;
+    //     let playlistName = req.query.playlistName;
+    //     let id = hash.MD5({name: req.query.name, artist: req.query.artist, album: req.query.album})
 
-        User.findOne({name: userName})
-            .then( user => {
-                if(!user){
-                    res.status(404).send("User not found!");
-                }else{
-                    // res.send(user);
-                    user.findOne({ playlists: {$elemMatch: {name: playlistName}}})
-                        .then(playlist => {
-                            if(!playlist) res.send("chacha")
-                            res.send("asdasdasd");
-                        })
-                        .catch( err => {
-                            res.send("err")
-                        })
-                    // let songid = hash.MD5({name: req.query.name, artist: req.query.artist, album: req.query.album})
-                    // Song.findOne({songId: songid})
-                    //     .then( song => {
-                    //         if(song){
-                    //             user.playlists.songs.push(song.id);
-                    //             res.send(user);
-                    //         }else{
-                    //             res.send(user)
-                    //         }
-                    //     })
-                    //     .catch( err => {
-                    //         res.send(err);
-                    //     })
-                }
-            })
-            .catch( err => {
-                let error = "eroare dracu";
-                res.send(error);
-            })
-    })
+    //     User.findOne({name: userName})
+    //         .then( user => {
+    //             if(!user){
+    //                 res.status(404).send("User not found!");
+    //             }else{
+    //                 // res.send(user);
+    //                 user.findOne({ playlists: {$elemMatch: {name: playlistName}}})
+    //                     .then(playlist => {
+    //                         if(!playlist) res.send("chacha")
+    //                         res.send("asdasdasd");
+    //                     })
+    //                     .catch( err => {
+    //                         res.send("err")
+    //                     })
+    //                 // let songid = hash.MD5({name: req.query.name, artist: req.query.artist, album: req.query.album})
+    //                 // Song.findOne({songId: songid})
+    //                 //     .then( song => {
+    //                 //         if(song){
+    //                 //             user.playlists.songs.push(song.id);
+    //                 //             res.send(user);
+    //                 //         }else{
+    //                 //             res.send(user)
+    //                 //         }
+    //                 //     })
+    //                 //     .catch( err => {
+    //                 //         res.send(err);
+    //                 //     })
+    //             }
+    //         })
+    //         .catch( err => {
+    //             res.send(error);
+    //         })
+    // })
 }
